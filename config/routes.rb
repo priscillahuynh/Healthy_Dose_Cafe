1aeca8dc2a645cdf4d83c255c5a02e3835395e56
priscillahuynh <priscillamhuynh@gmail.com>
2020-10-24 09:20:30 +0800

    add sessions welcome, login and signup routes, and user signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get '/' => 'sessions#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
+  
   resources :categories
   resources :orders
   resources :line_items
