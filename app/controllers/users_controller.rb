ded5b31fa4859497c542dccb4bb5fced1d3a1cf1
priscillahuynh <priscillamhuynh@gmail.com>
2020-10-24 09:43:05 +0800

    define create and user_params methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def create
+        @user = User.new(user_params)
+        if @user.save 
+            session[:user_id] = @user.id
+            redirect_to categories_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
