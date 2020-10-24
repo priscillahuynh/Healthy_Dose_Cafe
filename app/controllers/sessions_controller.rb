2c2a9910f89bb58666927e4a0b72d3fa6f683fd2
priscillahuynh <priscillamhuynh@gmail.com>
2020-10-24 14:08:47 +0800

    edit create and destroy methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,21 @@ class SessionsController < ApplicationController
     end
 
     def new
-        
     end
 
     def create
-        session[:username] = params[:username]
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to categories_path
+        else
+            flash.now.alert = 'Invalid username or password'
+            render :new
+        end
     end
 
     def destroy
-        
+        session.delete(:user_id)
+        redirect to '/'
     end
 end
