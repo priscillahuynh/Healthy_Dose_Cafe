700997e0ab2a1d592b77726ae93feeb45232a4bd
priscillahuynh <priscillamhuynh@gmail.com>
2020-10-24 18:07:20 +0800

    create custom setter and getter methods for category_name associated with product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,12 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :line_items
   has_many :carts, through: :line_items
+
+  def category_name=(name)
+    self.category = Category.find_or_create_by(name: name)
+  end
+
+  def category_name
+     self.category ? self.category.name : nil
+  end
 end
