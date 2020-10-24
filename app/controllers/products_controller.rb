0d225810dc6a920b935fe5347b4a7a3f9547c24b
priscillahuynh <priscillamhuynh@gmail.com>
2020-10-24 18:06:23 +0800

    define new, create, and product_params methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,18 @@
 class ProductsController < ApplicationController
+    def new
+        @product = Product.new
+    end
+
+    def create
+        @product = Product.new(product_params)
+        raise params.inspect
+
+    end
+
+    private
+
+    def product_params
+        params.require(:product).permit(:name, :price, :category_name)
+    end
+
 end
