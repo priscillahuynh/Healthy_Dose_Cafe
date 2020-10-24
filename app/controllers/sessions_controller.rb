c384cad79723345532f47b4a41ba80ac23535aa3
priscillahuynh <priscillamhuynh@gmail.com>
2020-10-24 10:39:05 +0800

    outline create and destroy methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,14 @@ class SessionsController < ApplicationController
     end
 
     def new
+        
     end
 
     def create
+        session[:username] = params[:username]
+    end
+
+    def destroy
+        
     end
 end
