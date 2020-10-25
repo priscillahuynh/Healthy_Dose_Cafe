4294bfbe253f6b57483f838c02cb14d42b94650b
priscillahuynh <priscillamhuynh@gmail.com>
2020-10-25 10:34:04 +0800

    add current user, logged_in?, and login_required methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user
-    helper_method :logged_in?
+    helper_method :current_user, :logged_in?
     
     def current_user
-        @current_user ||= User.find_by_id!(session[:user_id])
+        @current_user ||= User.find_by_id(session[:user_id])
     end
 
     def logged_in?
-        !!current_user
+        !!@current_user
     end
 
     def login_required
