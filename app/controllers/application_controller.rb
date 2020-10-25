2e5dca3a5309af69fa2fb51d1a280721290e295d
priscillahuynh <priscillamhuynh@gmail.com>
2020-10-25 16:42:23 +0800

    update current_user and logged_in? methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
     helper_method :current_user, :logged_in?
     
     def current_user
-        @current_user ||= User.find_by_id(session[:user_id])
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
 
     def logged_in?
-        !!@current_user
+        !!session[:user_id]
     end
 
     def login_required
