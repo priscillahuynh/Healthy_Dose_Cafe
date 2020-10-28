ca65e207fb440cb42decfe892a713b2a5b995349
priscillahuynh <priscillamhuynh@gmail.com>
2020-10-28 15:01:19 +0800

    add show and edit methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,19 +14,26 @@ class ProductsController < ApplicationController
         redirect_to category_products_path(@category)
     end
 
-    def show
-        @product = Product.find_by_id(params[:id])
+    def index
+        @category = Category.find_by_id(params[:category_id])
     end
 
-    def index
-        @products = Product.all
-        @category = Category.find_by_id(params[:category_id])
+    def show
+        find_product
+    end
+
+    def edit
+        find_product
     end
 
     private
 
     def product_params
         params.require(:product).permit(:name, :price, :category_name)
+    end
+
+    def find_product
+        @product = Product.find_by_id(params[:id])
     end
 
 end
