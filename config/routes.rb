45f3bfe4c52726f728b57f459176992c4f9c9d15
priscillahuynh <priscillamhuynh@gmail.com>
2020-10-28 14:28:44 +0800

    update nested resources for products

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   delete '/logout' => 'sessions#destroy'
   
+  resources :products
   resources :categories, :path => "menu" do
-    resources :products
+    resources :products, only: [:new, :show, :index]
   end
 
   resources :orders
   resources :line_items
   resources :carts
-  resources :products
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
