ed5850e0be3fe97f65d92acca6451c04a6c9e38e
priscillahuynh <priscillamhuynh@gmail.com>
2020-10-28 16:05:32 +0800

    modify create method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,16 @@ class CategoriesController < ApplicationController
     def new
         @category = Category.new
     end
+
+    def create 
+        @category = Category.new(params.require(:category).permit(:name))
+        if @category.save 
+            redirect_to categories_path
+        else 
+            render :new 
+        end
+    end
+
     def index
         @categories = Category.all
     end
