e1b92f0fc1a9ad3252a73997ac13333e30636f3e
priscillahuynh <priscillamhuynh@gmail.com>
2020-10-29 13:49:50 +0800

    add category_id to product_params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -45,7 +45,7 @@ class ProductsController < ApplicationController
     private
 
     def product_params
-        params.require(:product).permit(:name, :price, :category_name)
+        params.require(:product).permit(:name, :price, :category_name, :category_id)
     end
 
     def find_product
