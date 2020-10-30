a1c4e25e175a3d4782e3ec544d6dcf183ad6718f
priscillahuynh <priscillamhuynh@gmail.com>
2020-10-30 10:00:39 +0800

    nest cart resources under users

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
   end
   resources :orders
   resources :line_items
-  resources :carts
-  resources :users
+  resources :users do 
+    resources :carts
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
