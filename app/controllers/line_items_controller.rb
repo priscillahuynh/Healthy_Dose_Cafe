3a1937c86c3a4b8a126e14ae0bae4fc88c4abd36
priscillahuynh <priscillamhuynh@gmail.com>
2020-11-02 13:13:19 +0800

    add destroy, add_quantity, and reduce_quantity methods

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -13,4 +13,26 @@ class LineItemsController < ApplicationController
             redirect_to product_path(chosen_product), flash: { notice: "Product added to cart" }
     end
 
+    def destroy
+        @line_item = LineItem.find(params[:id])
+        @line_item.destroy
+        redirect_to cart_path(current_cart.id)
+    end
+
+    def add_quantity
+        @line_item = LineItem.find(params[:id])
+        @line_item.quantity += 1
+        @line_item.save
+        redirect_to cart_path(current_cart.id)
+      end
+      
+      def reduce_quantity
+        @line_item = LineItem.find(params[:id])
+        if @line_item.quantity > 1
+          @line_item.quantity -= 1
+        end
+        @line_item.save
+        redirect_to cart_path(current_cart.id)
+      end
+
 end
