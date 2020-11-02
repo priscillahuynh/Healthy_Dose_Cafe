ce59eb6bf7487db7026ee922e9db3a09fc223f3c
priscillahuynh <priscillamhuynh@gmail.com>
2020-11-02 13:14:27 +0800

    add url to increase and decrease line item quantity from cart

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,21 @@ Rails.application.routes.draw do
   get '/' => 'sessions#welcome'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+
   get '/signup' => 'users#new'
   delete '/logout' => 'sessions#destroy'
 
   get '/carts/:id' => "carts#show", as: "cart"
   delete '/carts/:id' => "carts#destroy"
 
+  post 'line_items/:id/add' => "line_items#add_quantity", as: "add_line_item"
+  post 'line_items/:id/reduce' => "line_items#reduce_quantity", as: "reduce_line_item"
+
   resources :products
   resources :categories, :path => "menu" do
     resources :products, only: [:new, :show, :index, :destroy]
   end
+
   resources :orders
   resources :line_items
   resources :users 
