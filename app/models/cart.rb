3ff208f09db6dce8e539908d537e72f84b306fc8
priscillahuynh <priscillamhuynh@gmail.com>
2020-11-03 14:47:12 +0800

    add optional to belongs_to relationship

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,6 @@
 class Cart < ApplicationRecord
   belongs_to :user
+  belongs_to :order, optional: true
   has_many :line_items, dependent: :destroy
   has_many :products, through: :line_items
 
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,7 @@
 class LineItem < ApplicationRecord
   belongs_to :product
-  belongs_to :cart
-
+  belongs_to :cart, optional:true
+  
   def total_price
     self.quantity * self.product.price
   end
